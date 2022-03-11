29aa828776129b6e1beb6ace47c5abd23e52ab73
peacepeng955 <100982142+peacepeng955@users.noreply.github.com>
2022-03-11 17:16:56 +0800

    Add files via upload

---
+++ Day01-15/code/Day02/variable1.py
@@ -0,0 +1,17 @@
+"""
+使用变量保存数据并进行操作
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-02-27
+"""
+
+a = 2
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
---
+++ Day01-15/code/Day02/variable3.py
@@ -0,0 +1,17 @@
+"""
+格式化输出
+
+Version: 0.1
+Author: 骆昊
+Date: 2018-02-27
+"""
+
+a = 3
+b = 4
+print('%d + %d = %d' % (a, b, a + b))
+print('%d - %d = %d' % (a, b, a - b))
+print('%d * %d = %d' % (a, b, a * b))
+print('%d / %d = %f' % (a, b, a / b))
+print('%d // %d = %d' % (a, b, a // b))
+print('%d %% %d = %d' % (a, b, a % b))
+print('%d ** %d = %d' % (a, b, a ** b))
